062fe0d3be40b16c17a594d9eacff319316a3d90
Alina Heghedus <alinaheghedus@yahoo.com>
2017-10-06 14:37:49 -0400

    add variables and makeGrid function

--- designs.js
+++ designs.js
@@ -1,10 +1,42 @@
-// Select color input
-// Select size input
+$(document).ready(function() {
+	console.log('ok');
 
-// When size is submitted by the user, call makeGrid()
+	
 
-function makeGrid() {
+	function makeGrid(h, w) {
+		console.log('test');
 
-// Your code goes here!
+		var table = $('#pixel_canvas');
+		$('#pixel_canvas').empty();
+		// Select color input
+		const color = $('#colorPicker').val();
+		// Select size input
+		const height = $('#input_height').val();
+		const width = $('#input_width').val();
+		console.log(color, height, width);
 
-}
+		let updateGrid = function (height, width) {
+			sketch = "";
+			for (let i=0; i < height; i++) {
+				sketch += "<tr>";
+				for (let j=0; j < width; j++) {
+					sketch += "<td></td>";
+				}
+				sketch += "</tr>";
+			};
+			return sketch;
+		}
+		$(table).append(updateGrid(height, width));
+	}
+
+	// When size is submitted by the user, call makeGrid()
+	$('#sizePicker').on('submit', function(e){
+		e.preventDefault();
+		makeGrid();
+	});	
+
+
+	//var canvas = $('#pixel_canvas');
+	//$(canvas).on('click', makeGrid());
+
+});
